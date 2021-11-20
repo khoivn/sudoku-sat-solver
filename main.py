5be1960f1ab2acf693a8125bc41af094fb5886ef
khoivn <vnkhoi76@gmail.com>
2021-11-20 12:12:52 +0700

    add example

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from SudokuSatSolver import SudokuSatSolver, EncodingMode
 
 if __name__ == '__main__':
-    BLOCK_SIZE = 4
-    FILE_NAME = "sudoku_16x16.txt"
+    BLOCK_SIZE = 7
+    FILE_NAME = "sudoku_49x49.txt"
+    METHOD = EncodingMode.PRODUCT
 
     clues = []
     with open(FILE_NAME, "r") as f:
         for line in f.readlines():
+            if not line.strip():
+                continue
             clue = line.strip().split("|")
             assert len(clue) == BLOCK_SIZE * BLOCK_SIZE
             clues.append(clue)
 
-    solver = SudokuSatSolver(BLOCK_SIZE, clues, EncodingMode.COMMANDER)
+    solver = SudokuSatSolver(BLOCK_SIZE, clues, METHOD)
     result = solver.solve()
     print(f"Method: {result.method}")
     print(f"Satisfiable: {result.satisfiable}")
